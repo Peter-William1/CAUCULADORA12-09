d5409c6e74d6b2ac033caae40f3ebc3b0c1de3fb
Dev Sistemas Tarde <dev-sistemas-tarde@lab.senai>
2023-09-12 14:22:20 -0300

    Aula-12-09

--- funcoes.py
+++ funcoes.py
@@ -1,8 +1,15 @@
-#funções
+#funcoes
+from datetime import datetime
+
 
 def saudacao():
-    print("bom dia")
-    print("boa tarde")
-    print("boa noite")
+    # hora = datetime.now().hour
+    hora =23
+    if hora>=0 and hora<=12:
+        print("Bom Dia!")
+    elif hora>17:
+        print("Boa Noite")
+    else:
+        print("Boa Tarde")
 
 saudacao()
