50ac75de068dcbb18bffe1a7a722f0375e5f6b5e
Dev Sistemas Tarde <dev-sistemas-tarde@lab.senai>
2023-09-12 16:47:43 -0300

    AULA-12-09

---
+++ f.py
@@ -0,0 +1,18 @@
+def soma(a,b):
+    print("soma:",a+b)
+
+def sub(a,b):
+    print("subtração:",a-b)
+
+def mult(a,b):
+    print("multiplicação:",a*b)
+
+def div(a,b):
+    print("divisão:",a/b)
+
+def pot(a,b):
+    print("potenciacão:",a ** b)
+
+def rad(a,b):
+    print("radiaciação:",a // b)
+
